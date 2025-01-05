50849db27b48ce9546732dd25284ffe912063df1
RoyelYl <yel_wang@bupt.edu.cn>
2025-01-05 22:58:41 +0800

    营销服务 第4节：策略权重概率装配

--- big-market-domain/src/main/java/cn/bugstack/domain/strategy/service/armory/IStrategyArmory.java
+++ big-market-domain/src/main/java/cn/bugstack/domain/strategy/service/armory/IStrategyArmory.java
@@ -2,7 +2,7 @@ package cn.bugstack.domain.strategy.service.armory;
 
 public interface IStrategyArmory {
 
-    void assembleLotteryStrategy(Long strategyId);
+    boolean assembleLotteryStrategy(Long strategyId);
 
-    Integer getRandomAwardId(Long strategyId);
+
 }
